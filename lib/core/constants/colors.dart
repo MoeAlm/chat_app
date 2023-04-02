085a1bd180d3fbac651dddabe7a4b8665e856099
MoeAlm <almazwzy8@gmail.com>
2023-04-03 00:01:58 +0200

    Edited

--- lib/core/constants/colors.dart
+++ lib/core/constants/colors.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 
 const kPrimaryColor = Color.fromRGBO(251, 196, 38, 1);
--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,11 +1,16 @@
 import 'package:chat_app/core/constants/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+import 'package:flutter_chat_bubble/chat_bubble.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
         backgroundColor: kPrimaryColor,
@@ -13,10 +18,33 @@ class ChatScreen extends StatelessWidget {
         automaticallyImplyLeading: false,
         title: Text(
           'Chat',
-          style: TextStyle(color: Colors.white),
+          style: TextStyle(
+              color: Colors.white,
+              fontSize: width * 0.085,
+              fontWeight: FontWeight.bold),
         ),
         centerTitle: true,
       ),
+      body: Column(
+        children: [
+          ChatBubble(
+            alignment: Alignment.topRight,
+            clipper: ChatBubbleClipper1(type: BubbleType.sendBubble),
+            child: const Text(
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+              style: TextStyle(color: Colors.white),
+            ),
+          ).py8(),
+          ChatBubble(
+            clipper: ChatBubbleClipper1(type: BubbleType.receiverBubble),
+            backGroundColor: kPrimaryColor,
+            child: Text(
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ],
+      ).px(12),
     );
   }
 }
