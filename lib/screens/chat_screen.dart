5a701ee591f4825aeb3c4bef40af0798b005d47c
MoeAlm <almazwzy8@gmail.com>
2023-04-11 19:24:27 +0200

    Edited

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,4 +1,5 @@
 import 'package:chat_app/core/constants/colors.dart';
+import 'package:chat_app/model/message_model.dart';
 import 'package:flutter/material.dart';
 import 'package:velocity_x/velocity_x.dart';
 
@@ -15,66 +16,85 @@ class ChatScreen extends StatelessWidget {
         FirebaseFirestore.instance.collection('Messages');
     TextEditingController controller = TextEditingController();
     double width = MediaQuery.of(context).size.width;
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: kPrimaryColor,
-        foregroundColor: Colors.white,
-        automaticallyImplyLeading: false,
-        title: Text(
-          'Chat',
-          style: TextStyle(
-              color: Colors.white,
-              fontSize: width * 0.085,
-              fontWeight: FontWeight.bold),
-        ),
-        centerTitle: true,
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: ListView.builder(
-              physics: const BouncingScrollPhysics(),
-              itemCount: 10,
-              itemBuilder: (BuildContext context, int index) => ChatBubble(
-                clipper: ChatBubbleClipper1(type: BubbleType.receiverBubble),
-                backGroundColor: kPrimaryColor,
-                child: const Text(
-                  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
-                  style: TextStyle(color: Colors.white),
+    return FutureBuilder<QuerySnapshot>(
+        future: messages.get(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            print(snapshot.data?.docs[0]['Message']);
+            List<Message> messagesList = [];
+            for(int i = 0; i < snapshot.data!.docs.length; i++){
+              messagesList.add(Message.fromJson(snapshot.data!.docs[i]));
+            }
+            return Scaffold(
+              appBar: AppBar(
+                backgroundColor: kPrimaryColor,
+                foregroundColor: Colors.white,
+                automaticallyImplyLeading: false,
+                title: Text(
+                  'Chat',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: width * 0.085,
+                      fontWeight: FontWeight.bold),
                 ),
-              ).py(10),
-            ).px(12),
-          ),
-          TextField(
-            controller: controller,
-            onSubmitted: (value) {
-              messages.add({
-                'Message': value,
-              });
-              controller.clear();
-            },
-            decoration: InputDecoration(
-              hintText: 'Send a Message',
-              suffixIcon: IconButton(
-                onPressed: () {},
-                icon: const Icon(
-                  Icons.send,
-                  color: kPrimaryColor,
-                ),
+                centerTitle: true,
               ),
-              contentPadding: const EdgeInsets.all(8),
-              border: OutlineInputBorder(
-                borderSide: const BorderSide(),
-                borderRadius: BorderRadius.circular(15),
-              ),
-              focusedBorder: OutlineInputBorder(
-                borderSide: const BorderSide(color: kPrimaryColor),
-                borderRadius: BorderRadius.circular(15),
-              ),
-            ),
-          ).pSymmetric(v: 6, h: 8)
-        ],
-      ).pOnly(bottom: 8),
-    );
+              body: Column(
+                children: [
+                  Expanded(
+                    child: ListView.builder(
+                      physics: const BouncingScrollPhysics(),
+                      itemCount: messagesList.length,
+                      itemBuilder: (BuildContext context, int index) =>
+                          ChatBubble(
+                            clipper:
+                            ChatBubbleClipper1(type: BubbleType.receiverBubble),
+                            backGroundColor: kPrimaryColor,
+                            child: Text(
+                              '${snapshot.data?.docs[index]['Message']}',
+                              style: const TextStyle(color: Colors.white),
+                            ),
+                          ).py(10),
+                    ).px(12),
+                  ),
+                  TextField(
+                    controller: controller,
+                    onSubmitted: (value) {
+                      messages.add({
+                        'Message': value,
+                      });
+                      controller.clear();
+                    },
+                    decoration: InputDecoration(
+                      hintText: 'Send a Message',
+                      suffixIcon: IconButton(
+                        onPressed: () {
+                          messages.add({
+                            'Message': controller.text,
+                          });
+                        },
+                        icon: const Icon(
+                          Icons.send,
+                          color: kPrimaryColor,
+                        ),
+                      ),
+                      contentPadding: const EdgeInsets.all(8),
+                      border: OutlineInputBorder(
+                        borderSide: const BorderSide(),
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(color: kPrimaryColor),
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                    ),
+                  ).pSymmetric(v: 6, h: 8)
+                ],
+              ).pOnly(bottom: 8),
+            );
+          }  else {
+            return Scaffold(body: Center(child: CircularProgressIndicator()));
+          }
+        });
   }
 }
